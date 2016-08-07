844488af91df07ac659a7aa878decb3b11e666cc
Chad Fegley <chadfegley7@gmail.com>
2016-08-07 00:35:28 -0700

    Earthquake Info Boxes

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var path = require("path");
 var bodyParser = require("body-parser");
 var topojson = require("topojson");
 // var config = require("/config_mapbox.js");
+var dateFormat = require('dateformat');
 var app = express();
 
 app.use(express.static(path.join(__dirname, "./client")));
